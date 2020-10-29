8327f4e7e42ee6e72a7f9bbb740d8fa4615e8a99
FlorencioP <sebasale10@hotmail.com>
2020-10-28 21:25:43 -0500

    Añadido resto de los datos del API a la PAG

--- JavaScript/menu.js
+++ JavaScript/menu.js
@@ -32,6 +32,10 @@ function rand() {
                 "<tr>" +
                 "<th class='tg-73oq'>Nombre</th>" +
                 "<th class='tg-73oq'>Categoria</th>" +
+
+                "<th class='tg-73oq'>Ingredientes</th>" +
+                //"<th class='tg-73oq'>Instrucciones</th>" +
+
                 "<th class='tg-73oq'>Origen</th>" +
                 "<th class='tg-73oq'>Imagen</th>" +
                 "</tr>" +
@@ -42,6 +46,37 @@ function rand() {
                 "<tr>" +
                 "<td class='tg-73oq'>" + datos.meals[0].strMeal + "</td>" +
                 "<td class='tg-73oq'>" + datos.meals[0].strCategory + "</td>" +
+
+
+
+
+                "<td class='tg-73oq'>";
+
+
+            if (datos.meals[0].strIngredient1 != "") { resultados += datos.meals[0].strIngredient1 + ": " + datos.meals[0].strMeasure1 + " <br> " };
+            if (datos.meals[0].strIngredient2 != "") { resultados += datos.meals[0].strIngredient2 + ": " + datos.meals[0].strMeasure2 + " <br> " };
+            if (datos.meals[0].strIngredient3 != "") { resultados += datos.meals[0].strIngredient3 + ": " + datos.meals[0].strMeasure3 + " <br> " };
+            if (datos.meals[0].strIngredient4 != "") { resultados += datos.meals[0].strIngredient4 + ": " + datos.meals[0].strMeasure4 + " <br> " };
+            if (datos.meals[0].strIngredient5 != "") { resultados += datos.meals[0].strIngredient5 + ": " + datos.meals[0].strMeasure5 + " <br> " };
+            if (datos.meals[0].strIngredient6 != "") { resultados += datos.meals[0].strIngredient6 + ": " + datos.meals[0].strMeasure6 + " <br> " };
+            if (datos.meals[0].strIngredient7 != "") { resultados += datos.meals[0].strIngredient7 + ": " + datos.meals[0].strMeasure7 + " <br> " };
+            if (datos.meals[0].strIngredient8 != "") { resultados += datos.meals[0].strIngredient8 + ": " + datos.meals[0].strMeasure8 + " <br> " };
+            if (datos.meals[0].strIngredient9 != "") { resultados += datos.meals[0].strIngredient9 + ": " + datos.meals[0].strMeasure9 + " <br> " };
+            if (datos.meals[0].strIngredient10 != "") { resultados += datos.meals[0].strIngredient10 + ": " + datos.meals[0].strMeasure10 + " <br> " };
+            if (datos.meals[0].strIngredient11 != "") { resultados += datos.meals[0].strIngredient11 + ": " + datos.meals[0].strMeasure11 + " <br> " };
+            if (datos.meals[0].strIngredient12 != "") { resultados += datos.meals[0].strIngredient12 + ": " + datos.meals[0].strMeasure12 + " <br> " };
+            if (datos.meals[0].strIngredient13 != "") { resultados += datos.meals[0].strIngredient13 + ": " + datos.meals[0].strMeasure13 + " <br> " };
+            if (datos.meals[0].strIngredient14 != "") { resultados += datos.meals[0].strIngredient14 + ": " + datos.meals[0].strMeasure14 + " <br> " };
+            if (datos.meals[0].strIngredient15 != "") { resultados += datos.meals[0].strIngredient15 + ": " + datos.meals[0].strMeasure15 + " <br> " };
+            if (datos.meals[0].strIngredient16 != "") { resultados += datos.meals[0].strIngredient16 + ": " + datos.meals[0].strMeasure16 + " <br> " };
+            if (datos.meals[0].strIngredient17 != "") { resultados += datos.meals[0].strIngredient17 + ": " + datos.meals[0].strMeasure17 + " <br> " };
+            if (datos.meals[0].strIngredient18 != "") { resultados += datos.meals[0].strIngredient18 + ": " + datos.meals[0].strMeasure18 + " <br> " };
+            if (datos.meals[0].strIngredient19 != "") { resultados += datos.meals[0].strIngredient19 + ": " + datos.meals[0].strMeasure19 + " <br> " };
+            if (datos.meals[0].strIngredient20 != "") { resultados += datos.meals[0].strIngredient20 + ": " + datos.meals[0].strMeasure20 + " <br> " };
+
+
+            resultados += "</td>" +
+                //"<td class='tg-73oq'>" + datos.meals[0].strInstructions + "</td>" +
                 "<td class='tg-73oq'>" + datos.meals[0].strArea + "</td>" +
                 "<td class='tg-73oq'> <img src=" + datos.meals[0].strMealThumb + " alt = 'imagen de la comida'> </td>" +
                 "</tr>" +
@@ -56,7 +91,7 @@ function rand() {
     xmlhttp.send();
 }
 
-
+var temp;
 
 function buscarPorcomida() {
     comidas = [];
@@ -92,12 +127,45 @@ function buscarPorcomida() {
 
             for (var i = 0; i < datos.meals.length; i++) {
 
-                comidas.push("<tr>" +
+
+                temp = "<tr>" +
                     "<td class='tg-73oq'>" + datos.meals[i].strMeal + "</td>" +
                     "<td class='tg-73oq'>" + datos.meals[i].strCategory + "</td>" +
+                    "<td class='tg-73oq'>";
+
+                if (datos.meals[i].strIngredient1 != "") { temp += datos.meals[i].strIngredient1 + ": " + datos.meals[i].strMeasure1 + " <br> " };
+                if (datos.meals[i].strIngredient2 != "") { temp += datos.meals[i].strIngredient2 + ": " + datos.meals[i].strMeasure2 + " <br> " };
+                if (datos.meals[i].strIngredient3 != "") { temp += datos.meals[i].strIngredient3 + ": " + datos.meals[i].strMeasure3 + " <br> " };
+                if (datos.meals[i].strIngredient4 != "") { temp += datos.meals[i].strIngredient4 + ": " + datos.meals[i].strMeasure4 + " <br> " };
+                if (datos.meals[i].strIngredient5 != "") { temp += datos.meals[i].strIngredient5 + ": " + datos.meals[i].strMeasure5 + " <br> " };
+                if (datos.meals[i].strIngredient6 != "") { temp += datos.meals[i].strIngredient6 + ": " + datos.meals[i].strMeasure6 + " <br> " };
+                if (datos.meals[i].strIngredient7 != "") { temp += datos.meals[i].strIngredient7 + ": " + datos.meals[i].strMeasure7 + " <br> " };
+                if (datos.meals[i].strIngredient8 != "") { temp += datos.meals[i].strIngredient8 + ": " + datos.meals[i].strMeasure8 + " <br> " };
+                if (datos.meals[i].strIngredient9 != "") { temp += datos.meals[i].strIngredient9 + ": " + datos.meals[i].strMeasure9 + " <br> " };
+                if (datos.meals[i].strIngredient10 != "") { temp += datos.meals[i].strIngredient10 + ": " + datos.meals[i].strMeasure10 + " <br> " };
+                if (datos.meals[i].strIngredient11 != "") { temp += datos.meals[i].strIngredient11 + ": " + datos.meals[i].strMeasure11 + " <br> " };
+                if (datos.meals[i].strIngredient12 != "") { temp += datos.meals[i].strIngredient12 + ": " + datos.meals[i].strMeasure12 + " <br> " };
+                if (datos.meals[i].strIngredient13 != "") { temp += datos.meals[i].strIngredient13 + ": " + datos.meals[i].strMeasure13 + " <br> " };
+                if (datos.meals[i].strIngredient14 != "") { temp += datos.meals[i].strIngredient14 + ": " + datos.meals[i].strMeasure14 + " <br> " };
+                if (datos.meals[i].strIngredient15 != "") { temp += datos.meals[i].strIngredient15 + ": " + datos.meals[i].strMeasure15 + " <br> " };
+                if (datos.meals[i].strIngredient16 != "") { temp += datos.meals[i].strIngredient16 + ": " + datos.meals[i].strMeasure16 + " <br> " };
+                if (datos.meals[i].strIngredient17 != "") { temp += datos.meals[i].strIngredient17 + ": " + datos.meals[i].strMeasure17 + " <br> " };
+                if (datos.meals[i].strIngredient18 != "") { temp += datos.meals[i].strIngredient18 + ": " + datos.meals[i].strMeasure18 + " <br> " };
+                if (datos.meals[i].strIngredient19 != "") { temp += datos.meals[i].strIngredient19 + ": " + datos.meals[i].strMeasure19 + " <br> " };
+                if (datos.meals[i].strIngredient20 != "") { temp += datos.meals[i].strIngredient20 + ": " + datos.meals[i].strMeasure20 + " <br> " };
+
+
+
+                temp += "</td>" +
                     "<td class='tg-73oq'>" + datos.meals[i].strArea + "</td>" +
                     "<td class='tg-73oq'> <img src=" + datos.meals[i].strMealThumb + " alt = 'imagen de la comida'> </td>" +
-                    "</tr>");
+                    "</tr>";
+
+                comidas.push(temp);
+
+
+
+
 
             }
 
@@ -147,6 +215,8 @@ function showcomidas(_comidas) {
         "<tr>" +
         "<th class='tg-73oq'>Nombre</th>" +
         "<th class='tg-73oq'>Categoria</th>" +
+        "<th class='tg-73oq'>Ingredientes</th>" +
+        //"<th class='tg-73oq'>Instrucciones</th>" +
         "<th class='tg-73oq'>Origen</th>" +
         "<th class='tg-73oq'>Imagen</th>" +
         "</tr>" +
